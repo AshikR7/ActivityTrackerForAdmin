807eeaabfa06a0be27418012ae34add08110ae21
Ashik Rajan <ashikrajan7@gmail.com>
2023-12-08 16:57:05 +0530

    Update activityTracker.py

--- activityTracker.py
+++ activityTracker.py
@@ -1,26 +1,23 @@
-
 import datetime
 class Activity_Tracker:
     def __init__(self):
         self.activity_log = []
 
-    def log_activity(self, user, action):
+    def log_activity(self, admin_name, action):
         timestamp = datetime.datetime.now()
         log_entry = {
             'timestamp': timestamp,
-            'user': user,
+            'admin': admin_name,
             'action': action
         }
         self.activity_log.append(log_entry)
 
     def view_activity_log(self):
         for entry in self.activity_log:
-            print(f"{entry['timestamp']} - User '{entry['user']}' performed '{entry['action']}'")
+            print(f"{entry['timestamp']} - Admin Name '{entry['admin']}' performed '{entry['action']}'")
 
 activity_tracker = Activity_Tracker()
 
-activity_tracker.log_activity("Admin", "Logged in")
-activity_tracker.log_activity("Admin","Change settings")
-activity_tracker.log_activity("Admin", "Logged out")
-
-activity_tracker.view_activity_log()
+activity_tracker.log_activity("Ashik", "Logged in")
+activity_tracker.log_activity("Ashik","Change settings")
+activity_tracker.log_activity("Ashik", "Logged out")
