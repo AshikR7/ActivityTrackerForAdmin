a141117cd80ac90669762051782c6bb5ba30aee4
Ashik Rajan <ashikrajan7@gmail.com>
2023-12-09 19:15:19 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,16 +1,22 @@
 from activityTracker import Activity_Tracker
 
 def log_In(name,action):
+    # log In method is defined here
+    
     activity_tracker = Activity_Tracker()
     activity_tracker.log_activity(name,action)
     activity_tracker.view_activity_log()
 
 def log_out(name,action):
+    # log Out method is defined here
+    
     activity_tracker = Activity_Tracker()
     activity_tracker.log_activity(name,action)
     activity_tracker.view_activity_log()
 
 def activating(name,action):
+    # activating method is defined here
+    
     activity_tracker = Activity_Tracker()
     activity_tracker.log_activity(name,action)
     activity_tracker.view_activity_log()
